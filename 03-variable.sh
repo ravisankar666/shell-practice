ff2fabae23ec6f09f90fb5e0dd3f3af43197eff3
ravisankar666 <ravisankarp666@gmail.com>
2025-09-18 21:43:16 +0530

    something

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#no space ,before and after equal to
+
+person1=roy
+person2=lisa
+
+echo " $person1 :: hi $person2 , how are you doing !"
+echo "$person2 :: hello $person1 , i am fine , how are you !"
+echo "$person1:: what are you learning recently"
+echo "$person2:: i am learning shell scripting $person1 "  
