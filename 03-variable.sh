4fa2af889489d12f8f1c682c2563435902360f43
ravisankar666 <ravisankarp666@gmail.com>
2025-09-21 20:47:25 +0530

    some message

--- 03-variable.sh
+++ 03-variable.sh
@@ -8,4 +8,4 @@ person2=lisa
 echo " $person1 :: hi $person2 , how are you doing !"
 echo "$person2 :: hello $person1 , i am fine , how are you !"
 echo "$person1:: what are you learning recently"
-echo "$person2:: i am learning shell scripting $person1 "  
+echo "$person2:: i am learning shell scripting $person1 "
