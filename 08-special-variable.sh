5db98f23a71568956192b484168faa62b6d9cb99
ravisankar666 <ravisankarp666@gmail.com>
2025-09-21 21:49:48 +0530

    some message

--- 08-special-variable.sh
+++ 08-special-variable.sh
@@ -3,9 +3,9 @@
 echo " all variable passed to the script : $@"
 echo " all variable passed to the script : $*"
 echo " script name : $0"
-echo " current directory : $pwd"
-echo " who is running this : $user"
-echo " Home directory of the user: $home "
+echo " current directory : $PWD"
+echo "Who is running this: $USER"
+echo " Home directory of the user: $HOME "
 echo " pid of the script : $$"
 #print process id 
 sleep 10 &   # basck ground running this process
