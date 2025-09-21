24dfb8f200a92254a2b218691d0fc5d6a46fa46e
ravisankar666 <ravisankarp666@gmail.com>
2025-09-21 20:59:56 +0530

    some message

---
+++ 05-read_com.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " please enter your oin number"
+
+read PIN_NUMBER  #here PIN_NUMBER is a variable.
+
+# if you put -s ----> pin_number is not visible ,while entering pin number.
