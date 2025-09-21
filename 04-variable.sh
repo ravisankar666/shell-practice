51ba0c2ec9c4ac19392f958ad6942061bc59136f
ravisankar666 <ravisankarp666@gmail.com>
2025-09-21 20:53:48 +0530

    some message

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+#Arguments
+
+person1=$1
+person2=$2
+
+echo " $person1 :: welcome to alaska $person2"
+echo " $person2 :: thank you $person1 , not on the zoom meeting "
+echo " $person1 :: so, do we have a deal "
+echo "$person2 :: yes, if you make ukraine join russia "
