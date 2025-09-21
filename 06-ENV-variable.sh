083ecc9a81eeef51c0ae637b59c6e46bf32c8efc
ravisankar666 <ravisankarp666@gmail.com>
2025-09-21 21:10:48 +0530

    some message

---
+++ 06-ENV-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# export course="devops with aws "--this variable is vaild until this process is alive, it is accessable to sub procress also
+
+# ./bashrc--> enter this variable with value in this file and source .bashrc
+
+
+echo " course name : $course"
+# if you exit server and enter you able run this variable , this variable and value is fixed in that instance.
