ef3654f0ea9e6d3aed6b7356ffff4f3452e74a36
ravisankar666 <ravisankarp666@gmail.com>
2025-09-21 21:53:11 +0530

    some message

--- 09-Datatypes.sh
+++ 09-Datatypes.sh
@@ -2,18 +2,17 @@
 
 # everything in shell consider as string
 
-number1=100
-number2=200
-number3=devops
+NUMBER1=100
+NUMBER2=200
+NAME=DevOps
 
-sum=$(($number1+$number2+$number3))  # shell has understand number3 variable and consider as string == zero.
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
-echo " sum is : $ sum "
-# size=3 MAX_index=2
+echo "SUM is: ${SUM}"
 
-#          0      1         2            position alway start with zero
-LEADERA=("modi" "putin" " trump") 
+# Size = 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
 
-echo " all leaders : ${LEADERS[@]}". # @ mean all , print everything .
-
-echo " all leaders : ${LEADERS[0]}"
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}"
