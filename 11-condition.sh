accc4675e5c9784977cd1b24e2b7bb96e40e2a09
ravisankar666 <ravisankarp666@gmail.com>
2025-09-25 15:13:33 +0530

    some message

---
+++ 11-condition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " please enter the number"
+
+read  NUMBER
+
+if [$(($NUMBER%2)) -eq 0]; then 
+    echo "Given number $NUMBER is even"
+else 
+    echo "Given number $NUMBER is odd"
+fi
