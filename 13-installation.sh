75c08f9b696cd60f9e122154398ccd7c619636e2
ravisankar666 <ravisankarp666@gmail.com>
2025-09-25 15:37:22 +0530

    some message

---
+++ 13-installation.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+
+if  [  $USERIS  -ne  0 ]; then 
+    echo “ Error :: please run this script with root privilege”
+    exit 1 # failure is other then zero
+fi
+
+dnf install mysql -y
+
+if  [  $?   -ne  0 ]; then 
+   echo “ error :: installing mysql is faillure”
+   exit 1
+else 
+   echo “ intsalling mysql is success”
+fi
+
+dnf install nginx —y
+
+if  [  $?  -ne  0 ]; then 
+   echo “ error :: installing nginx is faillure”
+   exit 1
+else 
+   echo “ intsalling nginx is success”
+fi
+
+dnf intsall mongodb-mongosh -y
+
+if  [ $?  -ne  0 ]; then 
+   echo “ error :: installing mongodb is faillure”
+   exit 1
+else 
+   echo “ intsalling mongodb is success”
+fi
+
+
