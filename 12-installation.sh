a627461b19da6b7e2b7a25736c3c3e6d6fb15f4f
ravisankar666 <ravisankarp666@gmail.com>
2025-09-25 15:28:56 +0530

    some message

---
+++ 12-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ USERID -ne 0 ]; then 
+   echo " Error :: please run the script with root privillage"
+   exit 1 #failure other than zero
+
+fi
+
+dnf intsall mysql -y
+
+if [ $? -ne 0 ]; then 
+   echo " error :: installing mysql failure"
+   exit 1
+else 
+    echo " installaing mysql success "
+fi
+
